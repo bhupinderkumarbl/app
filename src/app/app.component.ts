24d86d948cfa4ff245e180e961c05be236c64883
bhupinderkumarbl <bhupinder.kumar@blinfosoft.com>
2017-09-21 15:56:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent  {
+
+
+  showMenu(){
+    document.getElementById("navigation").classList.toggle('mobileMenu');
+  }
+
+
+}
+
+window.onscroll = function() {
+  // console.log(window.pageYOffset);
+  var nav = document.getElementById('navigation');
+  if ( window.pageYOffset > 40 ) {
+    nav.classList.add("navbar1");
+  } else {
+    nav.classList.remove("navbar1");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home-page/home.component';
+import {AboutComponent} from './about/about.component';
+import {Routes, RouterModule} from '@angular/router';
+import {notFound} from './404/404';
 
-import { AppComponent } from './app.component';
+
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+
+    {
+        path: 'about', component: AboutComponent,
+        children: [
+            {path: 'c', component: HomeComponent}
+        ]
+    },
+    {path: '**', component: notFound},
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AboutComponent,
+        notFound
+
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
